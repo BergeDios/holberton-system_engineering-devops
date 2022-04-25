5a3efa92b81fc580916b1c8e33a5b22fd35f6097
BergeDios <santiago.goyret@gmail.com>
2022-04-25 14:14:12 -0700

    task 2 ponele

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+exec { 'apt-get update':
+  provider  => shell,
+  command   => 'sudo apt-get -y update',
+  before    => Exec['sudo apt-get install -y nginx'],
+}
+
+exec { 'sudo apt-get install -y nginx':
+  provider  => shell,
+  command   => 'sudo apt-get install -y nginx',
+  before    => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider  => shell,
+  command   => "sudo sed -i '/^\tinclude /etc/nginx/sites-enabled/*;/a \n\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf",
+  before    => Exec['service nginx restart'],
+}
+
+exec { 'service nginx restart':
+  provider  => shell,
+  command   => 'sudo service nginx restart',
+}
